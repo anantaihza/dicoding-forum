9ea17134d4045d2a9dc96d5290b654f6f5cb3297
Ananta Ihza <anantaihza21@gmail.com>
2024-03-19 16:10:52 +0700

    chore: Set up initial project structure and ESLint configuration.

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'prettier'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { 
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest', 
+    sourceType: 'module' 
+  },
+  settings: { 
+    react: { version: 'detect' },
+  },
+  plugins: [
+    'react-refresh', 
+    'react',
+    'prettier'],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
